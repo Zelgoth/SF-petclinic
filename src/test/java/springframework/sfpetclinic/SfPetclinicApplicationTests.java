52dda8a47602ed87c0a54bacf82b7f39eb9f40af
Michael Collins <Zelgoth@live.com>
2020-06-30 17:26:17 -0500

    Initial Project Add

---
+++ src/test/java/springframework/sfpetclinic/SfPetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package springframework.sfpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfPetclinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
